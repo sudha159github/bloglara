32ad5c274109e530db89fc2bb1038679d8f21ce9
Sudha Ghimire <sudhaghimire159@gmail.com>
2026-02-23 16:37:19 +0545

    login code update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,10 +6,10 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    // public function home(Request $request){
-    //     $request->user()=='admin'
-    //     return view('dashboard');
-    // }
+    public function home(Request $request){
+        $request->user()=='admin'
+        return view('dashboard');
+    }
 
      public function index(){
         return view('admin.dashboard');
